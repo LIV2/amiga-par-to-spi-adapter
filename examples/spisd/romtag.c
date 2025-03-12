27bf97a53a4a961aa61183cbd93c72ab1b54b34d
Matt Harlum <matt@harlum.net>
2025-03-12 09:37:00 +0000

    Preliminary work to integrate mounter
    Still needs work to get the configdev as this is needed for kick to boot from a device

--- examples/spisd/romtag.c
+++ examples/spisd/romtag.c
@@ -1,10 +1,12 @@
 #include <exec/types.h>
 #include <exec/resident.h>
 #include <exec/nodes.h>
+#include <dos/dos.h>
 
 #include "version.h"
 
 extern ULONG auto_init_tables[];
+extern struct Library *init(__reg("a1") BPTR seg_list);
 
 LONG noexec(void) {
     return -1;
@@ -16,11 +18,11 @@ const struct Resident romtag =
     .rt_MatchWord = RTC_MATCHWORD,
     .rt_MatchTag = (void *)&romtag,
     .rt_EndSkip = &romtag + 1,
-    .rt_Flags = RTF_AUTOINIT,
+    .rt_Flags = RTF_COLDSTART,
     .rt_Version = VERSION,
     .rt_Type = NT_DEVICE,
     .rt_Pri = 0,
     .rt_Name = device_name,
     .rt_IdString = id_string,
-    .rt_Init = auto_init_tables
+    .rt_Init = init,
 };

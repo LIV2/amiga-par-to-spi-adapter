9fc9a7a75e31cf247b952d74b5dbb50bdcbe9369
Matt Harlum <matt@harlum.net>
2025-03-23 10:16:54 +0000

    Fixup build scripts and build for sf2000

--- examples/spisd/build.sh
+++ examples/spisd/build.sh
@@ -1,2 +1,2 @@
 set -x
-vc +aos68k -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootblock.S ../../spi-lib/spi.c ../../spi-lib/spi_low.asm -I../../spi-lib -O2 -nostdlib -lamiga -o spisd.device
+vc +aos68k -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib/spi.c ../../spi-lib/spi_low.asm -I../../spi-lib -O2 -nostdlib -lamiga -o spisd.device

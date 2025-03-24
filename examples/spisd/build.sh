b200765e0578cac2c61ad3f8fa4f485076f2863d
Matt Harlum <matt@harlum.net>
2025-03-24 10:21:38 +0000

    Add Autoconfig BootROM

--- examples/spisd/build.sh
+++ examples/spisd/build.sh
@@ -1,2 +1,3 @@
 set -x
-vc +aos68k -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib/spi.c ../../spi-lib/spi_low.asm -I../../spi-lib -O2 -nostdlib -lamiga -o spisd.device
+vc +aos68k -rmcfg-ldnodb -ldnodb="-Rstd -s -kick1x" -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib/spi.c ../../spi-lib/spi_low.asm -I../../spi-lib -O2 -nostdlib -lamiga -o spisd.device
+make -C bootldr

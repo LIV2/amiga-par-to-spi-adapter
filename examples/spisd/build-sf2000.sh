4acda8b7fdd69e3e09ba65fbeb283ae720b3de55
Matt Harlum <matt@harlum.net>
2025-03-24 10:21:38 +0000

    Add Autoconfig BootROM

--- examples/spisd/build-sf2000.sh
+++ examples/spisd/build-sf2000.sh
@@ -1,2 +1,3 @@
 set -x
-vc +aos68k -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib-sf2000/spi.c ../../spi-lib-sf2000/interrupt.asm -I../../spi-lib-sf2000 -O2 -nostdlib -lamiga -o spisd.device
+vc +aos68k -rmcfg-ldnodb -ldnodb="-Rstd -s -kick1x" -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib-sf2000/spi.c ../../spi-lib-sf2000/interrupt.asm -I../../spi-lib-sf2000 -O2 -nostdlib -lamiga -o spisd.device 
+make -C bootldr

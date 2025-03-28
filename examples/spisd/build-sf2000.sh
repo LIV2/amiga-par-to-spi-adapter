9041296171f992db335e2707fbada003b1f9aec8
Matt Harlum <matt@harlum.net>
2025-03-28 09:05:25 +0000

    Fixup bootrom

--- examples/spisd/build-sf2000.sh
+++ examples/spisd/build-sf2000.sh
@@ -2,6 +2,6 @@ set -x
 GREEN="\e[32;1m";
 RESET="\e[0m"
 echo -e "${GREEN}## Building spisd.device${RESET}"
-vc +aos68k -rmcfg-ldnodb -ldnodb="-Rstd -s -kick1" -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib-sf2000/spi.c ../../spi-lib-sf2000/interrupt.asm -I../../spi-lib-sf2000 -O2 -nostdlib -lamiga -o spisd.device && \ 
+vc +aos68k -rmcfg-ldnodb -ldnodb="-Rstd -s -kick1" -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib-sf2000/spi.c ../../spi-lib-sf2000/interrupt.asm -I../../spi-lib-sf2000 -O2 -nostdlib -lamiga -o spisd.device && \
 echo -e "${GREEN}## Building spisd boot rom${RESET}" && \
 make -C bootldr

0d7db9238ae1a7b5261b5790bee7c0f045e97640
Matt Harlum <matt@harlum.net>
2025-03-28 05:04:11 +0000

    Tweak build scripts

--- examples/spisd/build-sf2000.sh
+++ examples/spisd/build-sf2000.sh
@@ -1,3 +1,7 @@
 set -x
-vc +aos68k -rmcfg-ldnodb -ldnodb="-Rstd -s -kick1x" -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib-sf2000/spi.c ../../spi-lib-sf2000/interrupt.asm -I../../spi-lib-sf2000 -O2 -nostdlib -lamiga -o spisd.device 
+GREEN="\e[32;1m";
+RESET="\e[0m"
+echo -e "${GREEN}## Building spisd.device${RESET}"
+vc +aos68k -rmcfg-ldnodb -ldnodb="-Rstd -s -kick1" -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib-sf2000/spi.c ../../spi-lib-sf2000/interrupt.asm -I../../spi-lib-sf2000 -O2 -nostdlib -lamiga -o spisd.device && \ 
+echo -e "${GREEN}## Building spisd boot rom${RESET}" && \
 make -C bootldr
--- examples/spisd/build.sh
+++ examples/spisd/build.sh
@@ -1,3 +1,7 @@
 set -x
-vc +aos68k -rmcfg-ldnodb -ldnodb="-Rstd -s -kick1x" -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib/spi.c ../../spi-lib/spi_low.asm -I../../spi-lib -O2 -nostdlib -lamiga -o spisd.device
+GREEN="\e[32;1m";
+RESET="\e[0m"
+echo -e "${GREEN}## Building spisd.device${RESET}"
+vc +aos68k -rmcfg-ldnodb -ldnodb="-Rstd -s -kick1" -I$NDK32/Include_H romtag.c version.c device.c sd.c timer.c mounter.c bootpoint.S ../../spi-lib/spi.c ../../spi-lib/spi_low.asm -I../../spi-lib -O2 -nostdlib -lamiga -o spisd.device && \
+echo -e "${GREEN}## Building spisd boot rom${RESET}" && \
 make -C bootldr
